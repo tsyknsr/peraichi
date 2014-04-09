268ffd98b3c24940bc98474fe699ba9d2f513490
Kazuya Saito <saito@smart-algorithm.com>
2014-04-09 18:17:25 +0900

    Initial commit for `peraichi`

---
+++ app/Controller/AppUsersController.php
@@ -0,0 +1,78 @@
+<?php
+App::uses('UsersController', 'Users.Controller');
+
+class AppUsersController extends UsersController {
+
+	public $uses = array('Users.User');
+
+	public function add() {
+		if ($this->Auth->user()) {
+			$this->Session->setFlash(__d('users', 'You are already registered and logged in!'));
+			$this->redirect('/');
+		}
+
+		if (!empty($this->request->data)) {
+			$user = $this->User->register($this->request->data);
+			if ($user !== false) {
+				$Event = new CakeEvent(
+					'Users.Controller.Users.afterRegistration',
+					$this,
+					array(
+						'data' => $this->request->data,
+					)
+				);
+				$this->getEventManager()->dispatch($Event);
+				if ($Event->isStopped()) {
+					$this->redirect(array('action' => 'login'));
+				}
+
+				$this->redirect(array('action' => 'register', $this->User->data['User']['email_token']));
+
+				$this->_sendVerificationEmail($this->{$this->modelClass}->data);
+				$this->Session->setFlash(__d('users', 'Your account has been created. You should receive an e-mail shortly to authenticate your account. Once validated you will be able to login.'));
+				$this->redirect(array('action' => 'login'));
+			} else {
+				unset($this->request->data[$this->modelClass]['password']);
+				unset($this->request->data[$this->modelClass]['temppassword']);
+				$this->Session->setFlash(__d('users', 'Your account could not be created. Please, try again.'), 'default', array('class' => 'message warning'));
+			}
+		}
+	}
+
+	public function register($token) {
+		$this->autoRender = false;
+		$link = "/users/verify/email/{$token}";
+		echo 'register link<br />';
+		echo '<a href="' . $link . '">' . $link . '</a>';
+	}
+
+	public function verify($type = 'email', $token = null) {
+		if ($type == 'reset') {
+			// Backward compatiblity
+			$this->request_new_password($token);
+		}
+
+		try {
+			$this->{$this->modelClass}->verifyEmail($token);
+			$this->Session->setFlash(__d('users', 'Your e-mail has been validated!'));
+			return $this->redirect(array('action' => 'login'));
+		} catch (RuntimeException $e) {
+			$this->Session->setFlash($e->getMessage());
+			return $this->redirect('/');
+		}
+	}
+
+	public function render($view = null, $layout = null) {
+		if (is_null($view)) {
+			$view = $this->action;
+		}
+		$viewPath = substr(get_class($this), 0, strlen(get_class($this)) - 10);
+		if (!file_exists(APP . 'View' . DS . $viewPath . DS . $view . '.ctp')) {
+			$this->plugin = 'Users';
+		} else {
+			$this->viewPath = $viewPath;
+		}
+		return parent::render($view, $layout);
+	}
+
+}
